1cb1c89231b7858c5e7493cea67700ede0e34bbe
janvanderdoe <j.vdrdoe@tilburguniversity.edu>
2022-02-24 15:43:04 +0100

    Timing, num pages scraped, debugging and added code to import cities

--- web_scraper_indeed.py
+++ web_scraper_indeed.py
@@ -4,7 +4,30 @@ Created on Mon Feb 21 10:59:45 2022
 
 @author: janva
 """
+# import urllib library
+from urllib.request import urlopen
+  
+# import json
+import json
+# store the URL in url as 
+# parameter for urlopen
+url = "https://opendata.cbs.nl/ODataApi/OData/84734NED/Woonplaatsen"
 
+
+  
+# store the response of URL
+response = urlopen(url)
+  
+# storing the JSON response 
+# from url in data
+data_json = json.loads(response.read())
+  
+# print the json response
+for title in data_json:
+    print(title)
+
+
+####
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -13,13 +36,15 @@ import csv
 
 # "https://nl.indeed.com/vacatures?q=marketing&l=amsterdam&start=0"
 
-
-jobs=["marketing", "directeur"]
-job_locations = ["tholen", "rotterdam", "amsterdam", "tilburg"]
+#PARAMETERS
+jobs=["marketing", "leraar", "administrateur"]
+job_locations = ["rotterdam", "amsterdam", "tilburg"]
 max_pages = 3
 
-
+#Scraper
+start_time = datetime.now()
 data = []
+num_pages_scraped = 0
 for job in jobs:
     for job_location in job_locations:
         for page in range(0, max_pages):
@@ -28,7 +53,8 @@ for job in jobs:
             time.sleep(1)
             r = requests.get(url)
             soup = BeautifulSoup(r.text, "html.parser")
-            listings = soup.find_all(class_="slider_container")           
+            listings = soup.find_all(class_="slider_container")
+            num_pages_scraped += 1
             for listing in listings:
                 #Jobtitle
                 try:
@@ -45,12 +71,12 @@ for job in jobs:
                 bullets =[]
                 try:
                 
-                    for li in listings[0].find(class_ = "job-snippet").find_all("li"):
+                    for li in listing.find(class_ = "job-snippet").find_all("li"):
                         bullets.append(li.text)
                     description = '/'.join(bullets)
                 except:
                     description = ""
-                    
+                del bullets    
                 #Location
                 try:
                     location = listing.find(class_= "companyLocation").text
@@ -78,19 +104,23 @@ for job in jobs:
                             "Search_term_job" : job,
                             "Search_term_page" : page})
                 #Checks whether there's a next button. Break if there's not or no button at all
-                try:
-                    next_label = soup.find_all("nav")[2].find_all("a")[-1]["aria-label"]
-                    if next_label == "Volgende":
-                        pass
-                    else:
-                        break
-                except:
+            try:
+                next_label = soup.find_all("nav")[2].find_all("a")[-1]["aria-label"]
+                if next_label == "Volgende":
+                    pass
+                else:
                     break
+            except:
+                break
 
 #CSV writer
-with open("listings_indeed.csv", "w") as csv_file:
+with open("listings_indeed.csv", "w", encoding = "UTF-8") as csv_file:
     writer = csv.writer(csv_file, delimiter = ";")
     writer.writerow(["Title", "New", "Description", "location", "Salary", "Stars", "ST_location", "St_job", "ST_page", "Scrape_date"])
     now = datetime.now()
     for listing in data:
         writer.writerow([listing["Title"], listing["New"], listing["Description"], listing["location"], listing["Salary"], listing["Stars"], listing["Search_term_location"], listing["Search_term_job"], listing["Search_term_page"], now])
+
+end_time = datetime.now()
+print("Time it took to scrape: " + str(end_time - start_time))
+print("Number of pages scraped: " + str(num_pages_scraped))
